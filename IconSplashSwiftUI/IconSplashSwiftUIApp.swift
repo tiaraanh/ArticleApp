66a7bd51bf0d68a91b713acbec32177ba75de466
Tiara <tiaranurulh@gmail.com>
2022-11-29 19:43:50 +0700

    Initial Commit

---
+++ IconSplashSwiftUI/IconSplashSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconSplashSwiftUIApp.swift
+//  IconSplashSwiftUI
+//
+//  Created by Tiara H on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct IconSplashSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
